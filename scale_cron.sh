f52f2673a9dea5fc6e2073c09baf2aee29e25247
Rocco Muscaritolo <rocc1158@MHD2VUDV13.(none)>
2014-04-03 15:38:53 -0700

    Logging spacing

--- scale_cron.sh
+++ scale_cron.sh
@@ -8,12 +8,11 @@ maxclients=`sh /root/scaledown_check.sh | grep percent_maxclients | awk '{print 
 if awk -v ld=$load 'BEGIN {exit !(ld < 2)}' && awk -v mem=$mem 'BEGIN {exit !(mem < 30)}' && awk -v cli=$maxclients 'BEGIN {exit !(cli < 30)}'
 then
 	echo `date` "Autoscale load check: Scaling DOWN by 1 server!"
-        curl -X POST https://iad.autoscale.api.rackspacecloud.com/v1.0/execute/1/13aa6c05ca918b7f0cf6f04336176e4b2ba7359ab44f7f9b555a7a72b7d088b5/ 2> /dev/null
+        curl -X POST URLME 2> /dev/null
 else
         echo `date` "Autoscale load check: Not ready for scale down"
 fi
 
-echo "  Average sysload = $load
-        Average memory use = $mem %
-        Average MaxClients use = $maxclients %"
-
+echo "  Average sysload = $load"
+echo "  Average memory use = $mem %"
+echo "  Average MaxClients use = $maxclients %"
--- scale_cron_gen.sh
+++ scale_cron_gen.sh
@@ -7,11 +7,10 @@ mem=`sh /root/scaledown_check.sh | grep percent_memory_used | awk '{print $4}'`
 if awk -v ld=$load 'BEGIN {exit !(ld < 2)}' && awk -v mem=$mem 'BEGIN {exit !(mem < 30)}'
 then
 	echo `date` "Autoscale load check: Scaling DOWN by 1 server!"
-        curl -X POST https://iad.autoscale.api.rackspacecloud.com/v1.0/execute/1/13aa6c05ca918b7f0cf6f04336176e4b2ba7359ab44f7f9b555a7a72b7d088b5/ 2> /dev/null
+        curl -X POST URLME 2> /dev/null
 else
         echo `date` "Autoscale load check: Not ready for scale down"
 fi
 
-echo "  Average sysload = $load
-        Average memory use = $mem %"
-
+echo "  Average sysload = $load"
+echo "  Average memory use = $mem %"
