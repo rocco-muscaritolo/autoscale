102a63c65a740941d8d892f4b878ef6b7e399b66
Rocco Muscaritolo <rocc1158@MHD2VUDV13.(none)>
2014-04-03 13:08:51 -0700

    Removing curl transmit info from stderr in cron logs

--- scale_cron.sh
+++ scale_cron.sh
@@ -8,7 +8,7 @@ maxclients=`sh /root/scaledown_check.sh | grep percent_maxclients | awk '{print 
 if awk -v ld=$load 'BEGIN {exit !(ld < 2)}' && awk -v mem=$mem 'BEGIN {exit !(mem < 30)}' && awk -v cli=$maxclients 'BEGIN {exit !(cli < 30)}'
 then
 	echo `date` "Autoscale load check: Scaling DOWN by 1 server!"
-        curl -X POST https://iad.autoscale.api.rackspacecloud.com/v1.0/execute/1/13aa6c05ca918b7f0cf6f04336176e4b2ba7359ab44f7f9b555a7a72b7d088b5/
+        curl -X POST https://iad.autoscale.api.rackspacecloud.com/v1.0/execute/1/13aa6c05ca918b7f0cf6f04336176e4b2ba7359ab44f7f9b555a7a72b7d088b5/ 2> /dev/null
 else
         echo `date` "Autoscale load check: Not ready for scale down"
 fi
